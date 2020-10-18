2da755c166ad9f3899a8b2b71180e8f530500daf
asafbasali <73056413+asafbasali@users.noreply.github.com>
2020-10-18 15:21:22 +0300

    Update exercise 00.cs

--- exercise 00.cs
+++ exercise 00.cs
@@ -10,7 +10,7 @@ namespace exrcise_00
             for (int i=0; i < 5; i++){
                 Console.WriteLine("welcome");
             }
-            int a = 0
+            int a = 5
         }
     }
 }

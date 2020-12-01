8494afc47cf0613c923f8b947a4f3ea0e7f607e1
Levi Fichman <61022686+LTheCoder@users.noreply.github.com>
2020-12-01 11:53:54 +0200

    nothing

---
+++ dotnet5781_01_4850_7515/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet5781_01_4850_7515
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

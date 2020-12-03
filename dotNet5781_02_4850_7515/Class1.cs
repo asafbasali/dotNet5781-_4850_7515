b39eda0dae39284868b93c0740316472faf92263
Levi Fichman <61022686+LTheCoder@users.noreply.github.com>
2020-12-03 18:32:46 +0200

    התחלתי את תרגיל 2

---
+++ dotNet5781_02_4850_7515/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotNet5781_02_4850_7515
+{
+    class BusStaation
+    {
+        int sBusStationKey;
+        string station_location;
+        int Latitude;
+        int Longitude;
+
+
+    }
+}

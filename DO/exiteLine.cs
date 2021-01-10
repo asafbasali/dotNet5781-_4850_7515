7d6af412628b50bb81597bd2453cc72284f8ae0a
asafbasali12345@gmail.com <asafbasali12345@gmail.com>
2021-01-10 17:59:53 +0200

    DO

---
+++ DO/exiteLine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DO
+{
+    class exiteLine
+    {
+        internal int IDLinebus;
+        internal int exitLine;
+        internal int finishtime;
+    }  //internal int frequency;
+}

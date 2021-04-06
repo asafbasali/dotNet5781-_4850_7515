0e2af174c3773248f07b679db93a9b67ec16fc22
Levi Fichman <61022686+LTheCoder@users.noreply.github.com>
2021-04-06 14:52:44 +0300

    sure

--- DAL/DO/Station.cs
+++ DAL/DO/Station.cs
@@ -6,6 +6,15 @@ namespace DO
 {
     public class Station
     {
+        public Station(int _sBusStationKey, double _latitude, double _longitude, string _busstationaddres)
+        {
+            this.sBusStationKey = _sBusStationKey;
+            Latitude = _latitude;
+            Longitude = _longitude;
+            Busstationaddres = _busstationaddres;
+        }
+        public Station() { }
+
         public int sBusStationKey { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }

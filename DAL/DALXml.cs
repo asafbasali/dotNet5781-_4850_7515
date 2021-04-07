f697d726b783a3f039827a73f016217e5fcbb264
Levi Fichman <61022686+LTheCoder@users.noreply.github.com>
2021-04-07 17:58:30 +0300

    fgh

--- DAL/DALXml.cs
+++ DAL/DALXml.cs
@@ -29,7 +29,7 @@ namespace DAL
         }
         internal static readonly string path = Path.GetFullPath(
             Path.Combine(Directory.GetCurrentDirectory(), @"..\..\"))
-                            + @"XMLFiles\{0}.xml";
+                            + @"XMLFILES\{0}.xml";
         internal readonly string configPath = String.Format(path, "config");
         internal readonly string usersPath = String.Format(path, "Users");
         internal readonly string stationsPath = String.Format(path, "Stations");
